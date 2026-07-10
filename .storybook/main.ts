c7ac4bea69c82b8690a69022f2d2dc996c3b1dd9
Stephanie Cray <stephanie.cray@yahoo.com>
2026-07-10 11:33:31 -0400

    adding in storybook dependencies

---
+++ .storybook/main.ts
@@ -0,0 +1,16 @@
+import type { StorybookConfig } from '@storybook/angular-vite';
+
+const config: StorybookConfig = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-a11y",
+    "@storybook/addon-docs",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-vitest"
+  ],
+  "framework": "@storybook/angular-vite"
+};
+export default config;
